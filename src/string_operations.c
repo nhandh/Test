70e8365a3b16891f6246031540a4d680d26c47c4
Nhan Do <nhdo@nhdo.mtv.corp.google.com>
2018-06-23 21:05:19 -0700

    Added w_strlen function to string length of wchar_t string

--- src/string_operations.c
+++ src/string_operations.c
@@ -11,6 +11,15 @@ size_t my_strlen(const char *s)
    return (p - s);
 }
 
+size_t w_strlen(const wchar_t *s)
+{
+  const wchar_t *p = s;
+  while (*p)
+    ++p;
+
+  return (p -s);
+}
+
 char* my_strcpy(char* t, char* s)
 {
    char *p = t;
