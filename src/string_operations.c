6ba8c5ae19a50332229c199deae060b5e66b5ded
Nhan Do <nhdo@nhdo.mtv.corp.google.com>
2018-06-23 21:25:17 -0700

    Added strchr function for wide character string

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <wchar.h>
 
 // I am modifying this before stash.
 size_t my_strlen(const char *s)
@@ -9,6 +10,17 @@ size_t my_strlen(const char *s)
       ++p;
 
    return (p - s);
+}
+
+wchar_t *my_wstrchr(wchar_t *ws, wchar_t *wc)
+{
+  while (*ws)
+  {
+    if (*ws == wc)
+      return ws;
+    ++ws;
+  }
+  return NULL;
 }
 
 size_t my_wstrlen(const wchar_t *s)
